b53feea59326f1edc643f773cd7f8a88369b07f7
olemoo <olemoo.oleole@gmail.com>
2020-05-21 22:45:23 -0400

    Updates

---
+++ precache-manifest.d0a45d346328dee78a940d2406e647d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba00e77fc2af2b92a92195ac5945ebfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cee92fbd20108831be9",
+    "url": "/static/css/main.2a5da728.chunk.css"
+  },
+  {
+    "revision": "b62a2a094799cbdc7db8",
+    "url": "/static/js/2.57d625d6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.57d625d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cee92fbd20108831be9",
+    "url": "/static/js/main.3f8f9338.chunk.js"
+  },
+  {
+    "revision": "3324ef354fde8a07fe45",
+    "url": "/static/js/runtime-main.66866b15.js"
+  },
+  {
+    "revision": "d365925dcac380346b5b76846da20670",
+    "url": "/static/media/drink.d365925d.svg"
+  },
+  {
+    "revision": "9a031b01c90ddfaa886ac93e8b0e217e",
+    "url": "/static/media/footer-bg-desktop.9a031b01.jpg"
+  },
+  {
+    "revision": "7db8cdc5e8536a53c59a4f198b3a1e06",
+    "url": "/static/media/footer-bg-mobile.7db8cdc5.jpg"
+  },
+  {
+    "revision": "19238a714a0748aa770fe4e46a27c16e",
+    "url": "/static/media/footer-bg-tablet.19238a71.jpg"
+  },
+  {
+    "revision": "9b98aea38cd8218f209caf7481a62477",
+    "url": "/static/media/footer-fassai-logo.9b98aea3.png"
+  },
+  {
+    "revision": "27fb5484833c0ef145c43505a5c892a5",
+    "url": "/static/media/olemoo_logo.27fb5484.png"
+  }
+]);
